d8ef38a98be2ca883b276764cb11b4298ede970d
NotFelBRo <tagm554@gmail.com>
2026-05-29 22:09:54 -0300

    fazer correções de estilização na página

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const url = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+async function conversorMoedas(params) {
+  const url = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL",
+  );
+
+  conversor = await url.json();
+  conversorMoedas();
+}
