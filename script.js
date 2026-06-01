93ffcdb8f0a89a497f204a4da0f8850705c09c48
NotFelBRo <tagm554@gmail.com>
2026-06-01 19:40:28 -0300

    estrutura simples

--- script.js
+++
@@ -1,8 +0,0 @@
-async function conversorMoedas(params) {
-  const url = await fetch(
-    "https://economia.awesomeapi.com.br/json/last/USD-BRL",
-  );
-
-  conversor = await url.json();
-  conversorMoedas();
-}
