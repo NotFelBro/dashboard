0f2d42c6e1311b998b211894b8e2ce300862afaa
NotFelBRo <tagm554@gmail.com>
2026-06-02 20:48:15 -0300

    teste2

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-const url = document.getElementById("url");
-fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+document.getElementById("url");
+const url = fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
   });
-
-const elements = {
-  aside: document.querySelectorAll("aside"),
-  section: document.querySelectorAll("section"),
-};
-
-elements.aside.forEach((aside) => {
-  aside.addEventListener("click", (event) => {
-    event.preventDefault();
-    const id = event.target.id;
-    if (id) {
-      displayController(id);
-    }
-  });
-});
-
-function displayController(id) {
-  const targetClass = "secao-" + id;
-
-  elements.section.forEach((section) => {
-    section.classList.add("hidden");
-
-    if (section.classList.contains(targetClass)) {
-      section.classList.remove("hidden");
-    }
-  });
-}
