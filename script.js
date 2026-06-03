9babf7b1d325759f97e9984a1536a101bb6b101b
NotFelBRo <tagm554@gmail.com>
2026-06-02 22:03:15 -0300

    reserva

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const url = fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
   .then((data) => {
     console.log(data);
   });
+
+// reservado para criar o tema escuro/claro
+const btnDark = document.querySelector("#toggle-theme-dark");
+const btnLight = document.querySelector("#toggle-theme-light");
+
+const style = document.createElement("style");
+// reservado para criar o tema escuro/claro
