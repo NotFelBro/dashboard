d083bcf31892d8edd5345f32fd44f1cbad813bfd
NotFelBRo <tagm554@gmail.com>
2026-06-03 19:10:16 -0300

    adicionado uma função para mudar de ferramentas sem dar reload na página

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const url = fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
     console.log(data);
   });
 
+const lista = document.getElementById("evento-pai");
+const section = document.querySelectorAll("section");
+const botoes = document.querySelectorAll("#evento-pai button");
+
+lista.addEventListener("click", (e) => {
+  const id = e.target.id;
+  if (!id) return;
+
+  section.forEach((section) => {
+    section.classList.remove("ativa");
+    if (section.classList.contains("secao-" + id)) {
+      section.classList.add("ativa");
+    }
+  });
+
+  botoes.forEach((btn) => btn.classList.remove("ativo"));
+  e.target.classList.add("ativo");
+});
+
 // reservado para criar o tema escuro/claro
 const btnDark = document.querySelector("#toggle-theme-dark");
 const btnLight = document.querySelector("#toggle-theme-light");
